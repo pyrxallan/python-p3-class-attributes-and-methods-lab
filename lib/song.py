e6b752a3de76b5693c9486446753ea113278a443
Allan Ratemo <allanisratemo@gmail.com>
2025-11-19 12:19:10 +0300

    Initial

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,21 @@
 class Song:
-    pass
+    count = 0
+    genres = set()
+    artists = set()
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        Song.genres.add(self.genre)
+
+        Song.artists.add(self.artist)
+
+        Song.genre_count[self.genre] = Song.genre_count.get(self.genre, 0) + 1
+
+        Song.artist_count[self.artist] = Song.artist_count.get(self.artist, 0) + 1
